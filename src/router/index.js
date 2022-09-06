d1785493eab5ef7ff701982d710eb96533afb068
chenxianduan <1140387046@qq.com>
2022-09-06 19:17:39 +0800

    登入页面布局完成

--- src/router/index.js
+++ src/router/index.js
@@ -2,8 +2,14 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 
 Vue.use(VueRouter)
-
+// SPA
+// 首屏加载速度很慢
+// 路由懒加载 -使用到了路由懒加载，再去请求他
 const routes = [
+  {
+    path: '/login',
+    component: () => import('@/views/Login')
+  }
 ]
 
 const router = new VueRouter({
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,20 @@
 const { defineConfig } = require('@vue/cli-service')
+const { resolve } = require('path')
+const cpns = resolve(__dirname, 'src/components')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  lintOnSave: true,
+  configureWebpack: {
+    devServer: {
+      open: true,
+      port: 8888,
+      host: 'localhost'
+    },
+    // 路径别名
+    resolve: {
+      alias: {
+        cpns
+      }
+    }
+  }
 })
